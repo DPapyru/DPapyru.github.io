5449bf86092fc9e0465b293ec778163092c4225e
DPapyru <3577415213@qq.com>
2026-02-16 08:08:26 +0800

    add:小天使一气之下又在写框架了

--- site/content/ModsSource/DPapyru/MyAPIMod/SkillsSystem/Skill.cs
+++ site/content/ModsSource/DPapyru/MyAPIMod/SkillsSystem/Skill.cs
@@ -1,9 +1,11 @@
+using System;
 using System.Collections.Generic;
 
 namespace DPapyru.Core.SkillsSystem;
 
 /// <summary>
 /// 技能抽象类，用这个来负责管理技能
+/// 技能只干动作链接!
 /// </summary>
 public abstract class Skill
 {
@@ -15,19 +17,36 @@ public abstract class Skill
 /// <typeparam name="T"></typeparam>
 public abstract class Skill<T> : Skill where T : class
 {
+
+}
+
+/// <summary>
+/// 技能动作激活条件
+/// </summary>
+public record struct SkillsActionActiveCondition
+{
     
 }
 /// <summary>
-/// 测试语法
+/// 激活条件列表
 /// </summary>
-public class Combo1 : Skill
+public struct ActiveConditionList
 {
-    public override List<SkillAction> RegisterSkillAction()
+    /// <summary>
+    /// 检查列表
+    /// </summary>
+    public List<Func<bool>> CheckList;
+    /// <summary>
+    /// 添加检查委托
+    /// </summary>
+    /// <param name="func"></param>
+    /// <returns></returns>
+    public ActiveConditionList AddCondition(Func<bool> func)
     {
-        throw new System.NotImplementedException();
+        CheckList.Add(func);
+        return this;
     }
 }
-
 
 /*
 实际应该如此编写:
